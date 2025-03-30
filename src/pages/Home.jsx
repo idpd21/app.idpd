14a1bca706878232486304f6909d5858bbec3c94
idpd21 <jonut856@gmail.com>
2025-03-30 14:32:14 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import Route from "../components/Route";
+function Home() {
+  return (
+    <Route>
+      <div className=" text-white p-6 text-3xl h-screen">hello Home</div>
+    </Route>
+  );
+}
+export default Home;
