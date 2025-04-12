09042a1dc47dc713f091ea48ddca456a3679aa92
Your GitHub Username <jonut856@gmail.com>
2025-04-12 09:43:46 +0700

    update 3

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -61,12 +61,14 @@ useEffect(() => {
     <>
       <div className="bg-white h-screen w-screen flex justify-center items-center">
         <div className="bg-black p-10 md:w-1/2 w=[90%] rounded-lg text-white">
-          <div className="text-center flex">
-            <span className="text-2xl">ຮ້ານຂາຍອຸປະກອນການຮຽນພັດທະນາລາວ</span>
-            <h1>ຍິນດີຕ້ອນຮັບ</h1>
+          <div className="text-center flex text-2xl">
+            <img src="https://png.pngtree.com/png-clipart/20240212/original/pngtree-design-of-a-plaited-hexagon-logo-in-blue-created-as-vector-png-image_14300843.png" alt=""
+            className="w-16 h-16" />
+            <span className="">ຮ້ານຂາຍອຸປະກອນການຮຽນພັດທະນາລາວ</span>
           </div>
+          <div className="text-center"><h1>ຍິນດີຕ້ອນຮັບ</h1></div>
           <div>
-          <h2 className="text-lg"> ກະລຸນາປ້ອນຂໍ້ມູນເພື່ອ ເຂົ້າສູ່ລະບົບ</h2>
+          <h2 className="text-lg text-gray-500"> ກະລຸນາປ້ອນຂໍ້ມູນເພື່ອ ເຂົ້າສູ່ລະບົບ</h2>
           </div>
 
           <div>
