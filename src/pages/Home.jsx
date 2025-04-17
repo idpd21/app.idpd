f99547600edb070298891cc8636eedae499a3ba4
Your GitHub Username <jonut856@gmail.com>
2025-04-17 08:14:24 +0700

    update 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,9 @@ import Route from "../components/Route";
 function Home() {
   return (
     <Route>
-      <div className=" text-white p-6 text-3xl h-screen">hello Home</div>
+      <div className=" text-white p-6 text-3xl h-screen">
+        <img src="https://c.pxhere.com/images/94/ab/6a52c065c68b09ab6229ef81a3c9-1445891.jpg!d" alt="" className="h-screen w-auto"/>
+      </div>
     </Route>
   );
 }
