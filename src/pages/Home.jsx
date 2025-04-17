f4296432ebad41ee7dbf8f4563fd373381520360
Your GitHub Username <jonut856@gmail.com>
2025-04-17 08:26:31 +0700

    update 3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,13 @@ import Route from "../components/Route";
 function Home() {
   return (
     <Route>
-      <div className="flex items-center justify-center">
-        <div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="text-center flex justify-center items-center">
         <img src="https://png.pngtree.com/png-clipart/20240212/original/pngtree-design-of-a-plaited-hexagon-logo-in-blue-created-as-vector-png-image_14300843.png" alt=""
             className="w-32 h-32" />
             <span className="mt-4 text-4xl">ຮ້ານຂາຍອຸປະກອນການຮຽນພັດທະນາລາວ</span>
         </div>
-        <img src="https://i.pinimg.com/originals/f1/2c/8d/f12c8df7accde83f5e4e04bbc194f90d.jpg" alt="" className="h-64 w-64"/>
+        <img src="https://i.pinimg.com/originals/f1/2c/8d/f12c8df7accde83f5e4e04bbc194f90d.jpg" alt="" className="h-96 w-full rounded-lg "/>
       </div>
     </Route>
   );
