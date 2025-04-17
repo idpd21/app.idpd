670c3421372c883bbd96a9ddc3dcf480396c2733
Your GitHub Username <jonut856@gmail.com>
2025-04-17 08:41:10 +0700

    update 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,12 @@ function Home() {
   return (
     <Route>
       <div className="flex flex-col items-center justify-center">
-        <div className="text-center flex justify-center items-center">
+        <div className="text-center flex flex-col justify-center items-center">
         <img src="https://png.pngtree.com/png-clipart/20240212/original/pngtree-design-of-a-plaited-hexagon-logo-in-blue-created-as-vector-png-image_14300843.png" alt=""
             className="w-32 h-32" />
             <span className="mt-4 text-4xl">ຮ້ານຂາຍອຸປະກອນການຮຽນພັດທະນາລາວ</span>
         </div>
-        <div className="h=[80%] w=[90%] rounded-2xl"> <img src="https://i.pinimg.com/originals/f1/2c/8d/f12c8df7accde83f5e4e04bbc194f90d.jpg" alt="" className="h-full w-full rounded-xl px-8 py-4"/></div>
+        <div className="h=[80%] w-screen rounded-2xl"> <img src="https://i.pinimg.com/originals/f1/2c/8d/f12c8df7accde83f5e4e04bbc194f90d.jpg" alt="" className="h-full w-full rounded-xl px-8 py-4"/></div>
        
       </div>
     </Route>
