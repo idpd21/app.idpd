c1ce553c69b044367fb1932abe25bea187a98603
Your GitHub Username <jonut856@gmail.com>
2025-04-19 10:41:37 +0700

    update 16

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
             className="w-32 h-32" />
             <span className="mt-4 text-4xl">ຮ້ານຂາຍອຸປະກອນການຮຽນພັດທະນາລາວ</span>
         </div>
-        <div className="h=[70%] w=[90%]  border-2 border-black"> <img src="https://th.bing.com/th/id/R.c1624ded9f07e1bbc85ee91888b037ba?rik=F7uQOpxK36MxDw&riu=http%3a%2f%2fkiji.life%2fnewkiji%2fwp-content%2fuploads%2f2017%2f07%2fDSC_9452-1024x681.jpg&ehk=P65DhnyDEOIjNDG%2fFzVuOpg6%2fKz8TlcOp3h%2fuBnveLc%3d&risl=&pid=ImgRaw&r=0" alt="" className="h=[60%] w=[90%]"/></div>
+        <div className="  border-2 border-black"> <img src="https://th.bing.com/th/id/R.c1624ded9f07e1bbc85ee91888b037ba?rik=F7uQOpxK36MxDw&riu=http%3a%2f%2fkiji.life%2fnewkiji%2fwp-content%2fuploads%2f2017%2f07%2fDSC_9452-1024x681.jpg&ehk=P65DhnyDEOIjNDG%2fFzVuOpg6%2fKz8TlcOp3h%2fuBnveLc%3d&risl=&pid=ImgRaw&r=0" alt="" className="px-96 py-56"/></div>
        
       </div>
     </Route>
