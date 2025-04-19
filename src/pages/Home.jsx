c2104447d5722719baaa98e96c96515f05e36f23
Your GitHub Username <jonut856@gmail.com>
2025-04-19 10:56:07 +0700

    update 17

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,9 @@ function Home() {
         <div className="text-center flex flex-col justify-center items-center">
         <img src="https://png.pngtree.com/png-clipart/20240212/original/pngtree-design-of-a-plaited-hexagon-logo-in-blue-created-as-vector-png-image_14300843.png" alt=""
             className="w-32 h-32" />
-            <span className="mt-4 text-4xl">ຮ້ານຂາຍອຸປະກອນການຮຽນພັດທະນາລາວ</span>
+            <span className="text-4xl">ຮ້ານຂາຍອຸປະກອນການຮຽນພັດທະນາລາວ</span>
         </div>
-        <div className=" py-4 border-2 border-black"> <img src="https://th.bing.com/th/id/R.c1624ded9f07e1bbc85ee91888b037ba?rik=F7uQOpxK36MxDw&riu=http%3a%2f%2fkiji.life%2fnewkiji%2fwp-content%2fuploads%2f2017%2f07%2fDSC_9452-1024x681.jpg&ehk=P65DhnyDEOIjNDG%2fFzVuOpg6%2fKz8TlcOp3h%2fuBnveLc%3d&risl=&pid=ImgRaw&r=0" alt="" className="h-96 w-full"/></div>
+        <div className=" mt-4 border-2 border-black"> <img src="https://th.bing.com/th/id/R.c1624ded9f07e1bbc85ee91888b037ba?rik=F7uQOpxK36MxDw&riu=http%3a%2f%2fkiji.life%2fnewkiji%2fwp-content%2fuploads%2f2017%2f07%2fDSC_9452-1024x681.jpg&ehk=P65DhnyDEOIjNDG%2fFzVuOpg6%2fKz8TlcOp3h%2fuBnveLc%3d&risl=&pid=ImgRaw&r=0" alt="" className="h-96 w-full"/></div>
        
       </div>
     </Route>
